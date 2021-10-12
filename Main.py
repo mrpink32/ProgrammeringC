bd114d953f5478c2477ddbffb2ca985220a4b1b3
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-10-12 17:00:57 +0200

    Hmmmm

--- Main.py
+++ Main.py
@@ -3,10 +3,12 @@ from Tasks import QuadraticEquation
 from Tasks import CPRChecker
 import sys
 
+
 operations = [1, 2, 3, 4]
 
 
 def main():
+    global operations
     print(f"commands: \ntype {operations[0]} for Quadratic formula."
           f"\ntype {operations[1]} to generate a table of a given length."
           f"\ntype {operations[2]} to validate a CPR-number."
@@ -14,17 +16,17 @@ def main():
     while True:
         try:
             operation = int(input("choose an operation: "))
-            match operation
-                case operations[0]:
+            match operation:
+                case 1:
                     QuadraticEquation.quadraticFormula()
                     Retry(QuadraticEquation.quadraticFormula)
-                case operations[1]:
+                case 2:
                     Table.table()
                     Retry(Table.table)
-                case operations[2]:
+                case 3:
                     CPRChecker.CPRVerifier()
                     Retry(CPRChecker.CPRVerifier)
-                case operations[3]:
+                case 4:
                     exit()
                 case _:
                     print("Illegal action, input has to be a valid number...")
@@ -34,14 +36,17 @@ def main():
 
 def Retry(func):
     while True:
-        command = str(input("try again? [y/n]: "))
-        match command:
-            case "y":
-                func()
-            case "n":
-                break
-            case _:
-                print("Illegal action, input not valid...")
+        try:
+            command = str(input("try again? [y/n]: "))
+            match command:
+                case "y":
+                    func()
+                case "n":
+                    break
+                case _:
+                    print("Illegal action, input has to be a valid number...")
+        except Exception as e:
+            print(f"Illegal action, input has to be a valid number... {e}")
 
 
 if __name__ == "__main__":
