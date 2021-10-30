04faaa08290112b340b2d739ceef2d7dd34a4523
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-10-31 00:16:47 +0200

    added offset to fix the definition of the top

--- Tasks/AnalogWatch.py
+++ Tasks/AnalogWatch.py
@@ -28,10 +28,10 @@ def update(root, canvas):
     timeSecond = returnTime()[2]
     print(f"Time: {timeHour, timeMinute, timeSecond}")
     canvas.create_oval(size - 50, size - 50, size - 750, size - 750, width=5, fill="yellow")
-    canvas.create_line(size/2, size/2 - 50, size/2, size/2 - 100, width=3, fill="#ffffff")
-    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(hour=timeHour)[0], size/2+returnPalcementWeights(hour=timeHour)[1], width=5, fill="black")
-    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(minute=timeMinute)[2], size/2+returnPalcementWeights(minute=timeMinute)[3], width=3, fill="black")
-    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(second=timeSecond)[4], size/2+returnPalcementWeights(second=timeSecond)[5], width=1, fill="black")
+    canvas.create_line(size/2, 100, size/2, 45, width=3, fill="#ffffff")
+    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(hour=timeHour)[0], size/2+returnPalcementWeights(hour=timeHour)[1], width=5, fill="#00ff00")
+    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(minute=timeMinute)[2], size/2+returnPalcementWeights(minute=timeMinute)[3], width=3, fill="#ff0000")
+    canvas.create_line(size/2, size/2, size/2+returnPalcementWeights(second=timeSecond)[4], size/2+returnPalcementWeights(second=timeSecond)[5], width=1, fill="#000000")
     root.after(1000, update, root, canvas)
 
 
@@ -39,20 +39,21 @@ def returnPalcementWeights(hour=0, minute=0, second=0):
     vinkelHour = 90-(360/12)*(math.pi/180)*hour
     vinkelMinute = 90-(360/60)*(math.pi/180)*minute
     vinkelSecond = 90-(360/60)*(math.pi/180)*second
-    print(f"Angle: {vinkelHour, vinkelMinute, vinkelSecond}")
-    xWeightHour = math.cos(-vinkelHour) * handSizes[0]
-    yWeightHour = math.sin(-vinkelHour) * handSizes[0]
-    xWeightMinute = math.cos(-vinkelMinute) * handSizes[1]
-    yWeightMinute = math.sin(-vinkelMinute) * handSizes[1]
-    xWeightsecond = math.cos(-vinkelSecond) * handSizes[2]
-    yWeightsecond = math.sin(-vinkelSecond) * handSizes[2]
+    #print(f"Angle: {vinkelHour, vinkelMinute, vinkelSecond}")
+    xWeightHour = math.cos(-vinkelHour+0.465) * handSizes[0]
+    yWeightHour = math.sin(-vinkelHour+0.465) * handSizes[0]
+    xWeightMinute = math.cos(-vinkelMinute+0.465) * handSizes[1]
+    yWeightMinute = math.sin(-vinkelMinute+0.465) * handSizes[1]
+    xWeightsecond = math.cos(-vinkelSecond+0.465) * handSizes[2]
+    yWeightsecond = math.sin(-vinkelSecond+0.465) * handSizes[2]
     #print(xWeightHour, yWeightHour, xWeightMinute, yWeightMinute, xWeightsecond, yWeightsecond)
     return xWeightHour, yWeightHour, xWeightMinute, yWeightMinute, xWeightsecond, yWeightsecond
 
 
 def returnTime():
     time = datetime.datetime.now()
-    return time.hour-12, time.minute, time.second if time.hour>12 else time.hour, time.minute, time.second
+    #print(f"{time.hour}")
+    return (time.hour-12, time.minute, time.second) if time.hour>12 else (time.hour, time.minute, time.second)
 
 
 if __name__ == "__main__":
