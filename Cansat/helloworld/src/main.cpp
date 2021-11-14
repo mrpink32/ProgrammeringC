d1743fbe37c3566287360d41642ae596c709d8b1
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-11-14 02:20:04 +0100

    ghjgj

---
+++ Cansat/helloworld/src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+
+
+void setup() {
+  // put your setup code here, to run
+  Serial.begin(115200);
+  ledcSetup(2, 5, 12);
+  ledcAttachPin(2, 2);
+  pinMode(1, OUTPUT);
+
+}
+
+int value = 1;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.printf("Dav: %d\n", value);
+  digitalWrite(1, LOW);
+  delay(2000);
+  digitalWrite(1, HIGH);
+  value++;
+}
