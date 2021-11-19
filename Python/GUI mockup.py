bf1a6b3232547c24b88f866583cded04e36d109f
TheWeirdTaco <93261428+TheWeirdTaco@users.noreply.github.com>
2021-11-19 14:07:46 +0100

    Created the window, frame, entry for cpr and an quit button
    It doesnt quite work properly:(

--- Python/GUI mockup.py
+++ Python/GUI mockup.py
@@ -1,1 +1,20 @@
 from tkinter import *
+#Thought is to make a CPR checker with only 5 widgets
+def main_root():
+    #Making and 
+    root = Tk()
+    root.geometry('300x300')
+    root.title('CPR validation program')
+    
+    frame = Frame(root,bg = "#d2d2d2",bd = 10)
+    frame.grid(row = 0, column = 0)
+
+    CPRentry = Entry(root,text = "insert your cpr number")
+    CPRentry.grid(row = 0, column = 0)
+
+    exitbutton = Button(frame,width = 4,height = 1, bg = "#ffffff", text = "Quit",command = exit)
+    exitbutton.grid(row = 3,column = 3) 
+
+    root.mainloop()
+
+main_root()
