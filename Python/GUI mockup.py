90e46d7dfd74e43afdd80106af63d79cf7ac0676
TheWeirdTaco <93261428+TheWeirdTaco@users.noreply.github.com>
2021-11-19 16:38:09 +0100

    problem with root.winfo_width

--- Python/GUI mockup.py
+++ Python/GUI mockup.py
@@ -1,19 +1,31 @@
 from tkinter import *
-#Thought is to make a CPR checker with only 5 widgets
+from tkinter import ttk
+#Thought is to make a CPR checker with 5 or less widgets
 def main_root():
-    #Making and 
     root = Tk()
-    root.geometry('300x300')
-    root.title('CPR validation program')
+    print(root.winfo_geometry())
+    #root.geometry('400x400')
+    print(root.winfo_geometry())
+    #root.title('CPR validation program')
     
-    frame = Frame(root,bg = "#d2d2d2",bd = 10)
-    frame.grid(row = 0, column = 0)
+    root.rowconfigure(0,weight = 1)
+    root.columnconfigure(0,weight = 1)
 
-    CPRentry = Entry(root,text = "insert your cpr number")
-    CPRentry.grid(row = 0, column = 0)
+    frame = Frame(root,bg = "#d2d2d2",width = 100,height = 100)
+    frame.grid(sticky = N+S+E+W)
 
-    exitbutton = Button(frame,width = 4,height = 1, bg = "#ffffff", text = "Quit",command = exit)
-    exitbutton.grid(row = 3,column = 3) 
+    #CPR_EUL = Label(frame,bg = "#555555",width = 10, height = int(10/3))
+    #CPR_EUL.grid(row = 0,column = 0)
+    CPRentry = Entry(root,text = "insert your cpr number",width = int(root.winfo_width()/2))
+    CPRentry.grid(row = 0, column = 0,sticky = N+W)
+    
+    #Entry doesnt quite work with .grid it seems, i think it doenst registre it as a object in the grid.
+    #CPRentry was supposed to be in the far left corner but the exit button just underlaps it. i might work around it. by placing a lable underneath.
+
+    
+
+    #exitbutton = Button(frame,width = 4,height = 1, bg = "#ffffff", text = "Quit",command = exit)
+    #exitbutton.grid 
 
     root.mainloop()
 
