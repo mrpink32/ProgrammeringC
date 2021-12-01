58ad0fb69b4d741d722d43bc3c6442fa0e56ece6
TheWeirdTaco <93261428+TheWeirdTaco@users.noreply.github.com>
2021-12-01 14:12:07 +0100

    glemte hvad jeg lavede
    glemte at push

--- Python/GUI mockup.py
+++ Python/GUI mockup.py
@@ -4,22 +4,19 @@ from tkinter import ttk
 #Thought is to make a CPR checker with 5 or less widgets
 def main_root():
     root = Tk()
-    print(root.winfo_geometry())
     root.geometry('400x400')
     root.update()
-    print(root.winfo_geometry())
-    #root.title('CPR validation program')
+    root.title('CPR validation program')
     
     root.rowconfigure(0,weight = 1)
     root.columnconfigure(0,weight = 1)
 
     frame = Frame(root,bg = "#d2d2d2",width = 100,height = 100)
     frame.grid(sticky = N+S+E+W)
-    print(root.winfo_geometry())
 
     #CPR_EUL = Label(frame,bg = "#555555",width = 10, height = int(10/3))
     #CPR_EUL.grid(row = 0,column = 0)
-    CPRentry = Entry(root,text = "insert your cpr number",width = int(root.winfo_width()/2))
+    CPRentry = Entry(root,text = "Insert CPR number" ,width = int(2*(root.winfo_width/3)))
     CPRentry.grid(row = 0, column = 0,sticky = N+W)
     print(root.winfo_geometry())
     
