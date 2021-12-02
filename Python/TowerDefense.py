4f48df69a94e0b4141d2e1553b5e857c5b4210a7
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-12-02 16:53:10 +0100

    added pics and merged

--- Python/TowerDefense.py
+++ Python/TowerDefense.py
@@ -3,10 +3,34 @@ from tkinter import ttk
 from math import *
 from PIL import Image, ImageTk
 
+
 class TDG(Frame):
     def __init__(self, master = None):
         Frame.__init__(self,master) 
         self.grid(sticky = N+S+E+W)
-
     def createboard():
+        pass
         
+
+class BasicEnemy:
+    def __init__(self, hitPoints, damageCount, moveSpeed):
+        self.hitPoints = hitPoints
+        self.damageCount = damageCount
+        self.moveSpeed = moveSpeed
+
+
+def main():
+    root = Tk()
+    app = TDG(root)
+    app.master.title("Shitty monkey ripoff")
+    gameLoop(app)
+    app.mainloop()
+
+
+def gameLoop(app):
+
+    app.after(100, gameLoop)
+
+
+if __name__ == "__main__":
+    main()
