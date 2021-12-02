2e8a367fd53dc5b2653899593400584a9d99ea2a
TheWeirdTaco <93261428+TheWeirdTaco@users.noreply.github.com>
2021-12-02 15:15:30 +0100

    created class and createboard

--- Python/TowerDefense.py
+++ Python/TowerDefense.py
@@ -2,3 +2,11 @@ from tkinter import *
 from tkinter import ttk
 from math import *
 from PIL import Image, ImageTk
+
+class TDG(Frame):
+    def __init__(self, master = None):
+        Frame.__init__(self,master) 
+        self.grid(sticky = N+S+E+W)
+
+    def createboard():
+        
