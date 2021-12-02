76301c94e53096f52d9b0920559f2d02c80b90ae
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-12-02 20:11:22 +0100

    trying to add picture

--- Python/TowerDefense.py
+++ Python/TowerDefense.py
@@ -1,16 +1,39 @@
 from tkinter import *
-from tkinter import ttk
 from math import *
 from PIL import Image, ImageTk
 
 
 class TDG(Frame):
     def __init__(self, master = None):
-        Frame.__init__(self,master) 
+        Frame.__init__(self, master) 
         self.grid(sticky = N+S+E+W)
-    def createboard():
-        pass
-        
+        self.createGrid()
+    def createGrid(self):
+        top = self.winfo_toplevel()
+        top.geometry("500x500")
+        top.rowconfigure(0, weight=1)
+        top.columnconfigure(0, weight=1)
+        for x in range(0, 99):
+            self.rowconfigure(x, weight=1)
+            self.columnconfigure(x, weight=1)
+        self.populateGrid()
+    def populateGrid(self):
+        #grassImg = Image.open("D:/HTX/ProgrammeringC/Python/TowerDefenseResources/Grass.png")
+        grassImg = PhotoImage(file="D:/HTX/ProgrammeringC/Python/TowerDefenseResources/Grass.png")
+        grassImg.configure(height=1, width=1)
+        grassTile = Label(self, image=grassImg)
+        grassTile.grid(row=1, column=1) #, sticky=N+S+E+W
+        print(grassTile.winfo_height(), grassTile.winfo_width())
+
+
+        #grassImg = Image.
+        #grassTile = ImageTk.BitmapImage(image=grassImg)
+        #grassTile.grid(row=1,column=1)
+
+
+
+
+
 
 class BasicEnemy:
     def __init__(self, hitPoints, damageCount, moveSpeed):
@@ -29,7 +52,7 @@ def main():
 
 def gameLoop(app):
 
-    app.after(100, gameLoop)
+    app.after(100, gameLoop, app)
 
 
 if __name__ == "__main__":
