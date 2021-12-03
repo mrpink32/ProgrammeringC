e169d624da00fae1be3ee2eec07e49bb67e40f57
Mikkel Paulick <mikkelpaulick@hotmail.com>
2021-12-03 08:31:13 +0100

    Kinda works

--- Python/TowerDefense.py
+++ Python/TowerDefense.py
@@ -13,16 +13,21 @@ class TDG(Frame):
         top.geometry("500x500")
         top.rowconfigure(0, weight=1)
         top.columnconfigure(0, weight=1)
-        for i in range(0, 99):
+        for i in range(0, 49):
             self.rowconfigure(i, weight=1)
             self.columnconfigure(i, weight=1)
         self.populateGrid()
     def populateGrid(self):
         # Why no work: https://www.youtube.com/watch?v=NoTM8JciWaQ
         grassImg = ImageTk.PhotoImage(Image.open("D:/HTX/ProgrammeringC/Python/TowerDefenseResources/Grass.png"))
-        grassTile = Label(self, bg="#5eff5e", text="test", height=10, width=10) #, image=grassImg
-        grassTile.grid(row=1, column=1) #, sticky=N+S+E+W
-        print(grassTile.winfo_height(), grassTile.winfo_width())
+        for i in range(0, 49):
+            for j in range(0, 49):
+                grassTile = Label(self, bg="#5eff5e", height=10, width=10, image=grassImg)
+                grassTile.grid(row=j, column=i, sticky=N+S+E+W)
+
+        #grassTile = Label(self, bg="#5eff5e", text="test", height=10, width=10, image=grassImg)
+        #grassTile.grid(row=0, column=0, sticky=N+S+E+W)
+        #print(grassTile.winfo_height(), grassTile.winfo_width())
 
 
 class BasicEnemy:
@@ -42,7 +47,7 @@ def main():
 
 def gameLoop(app):
 
-    app.after(100, gameLoop, app)
+    app.after(600, gameLoop, app)
 
 
 if __name__ == "__main__":
