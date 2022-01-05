755622655c45d8065d864d190c7dc216adea34fa
Mikkel Paulick <mikkelpaulick@hotmail.com>
2022-01-05 19:31:20 +0100

    images in canvas works kinda...

--- TowerDefense/TowerDefense.py
+++ TowerDefense/TowerDefense.py
@@ -8,7 +8,7 @@ class Application(Canvas):  # Application is a Canvas (inheritance from Canvas)
     def __init__(self, master):
         Canvas.__init__(self, master, width=500, height=500) 
         self.grid(sticky=N+S+E+W) # put canvas in toplevel window
-        self.populateCanvas()
+        #self.populateCanvas()
      
     def populateCanvas(self):
         img = PhotoImage(file="Resources\download.png")
@@ -16,20 +16,21 @@ class Application(Canvas):  # Application is a Canvas (inheritance from Canvas)
         self.create_line(0,0,50,50)
 
 
-
-
 def main():
     root = Tk()
     app = Application(root)
     app.master.title("Monkey")
+    img = PhotoImage(file="Resources\download.png")
+    app.create_image(0, 0, anchor=NW, image=img)
 
-    update(root, app)
+    update(app)
     root.mainloop()
 
-def update(root, app):
+
+def update(app):
    
 
-    root.after(17, update, root, app)
+    app.master.after(17, update, app)
 
 
 if __name__ == "__main__":  
