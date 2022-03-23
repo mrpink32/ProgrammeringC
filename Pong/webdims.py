5e2b4f594033d53d16af3b93f8dab7ce609645fd
Mikkel Paulick <mikkelpaulick@hotmail.com>
2022-03-23 16:30:00 +0100

    Update webdims.py

--- Pong/webdims.py
+++ Pong/webdims.py
@@ -4,14 +4,14 @@ import threading
 class Server:
     def __init__(self):
         websocket.enableTrace(True)
-        socket = "localhost"                 
+        socket = "localhost"
         ws = websocket.WebSocketApp(socket,
-                                                on_open=self.on_open)#, 
+                                                on_open=self.on_open)#,
                                                 #on_message=on_message,
-                                                #on_close=on_close) 
+                                                #on_close=on_close)
 
         wst = threading.Thread(target=lambda: ws.run_forever())
-        wst.daemon = True                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     
+        wst.daemon = True
         wst.start()
     def on_open(self):
         print("grim")
@@ -21,7 +21,7 @@ class Client:
     def __init__(self):
         self.test_socket = websocket.WebSocketApp("localhost", on_open=self.grim)
         #self.test_socket = websocket.create_connection("localhost")
-        
+
     def grim(self, message):
         print(message)
 
