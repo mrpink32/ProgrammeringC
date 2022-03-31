0b2c07cce5b23c603a3bacf7e09c56358bd0433e
Mikkel Paulick <mikkelpaulick@hotmail.com>
2022-03-31 18:56:51 +0200

    gaming

---
+++ Pong/lib/custom_networking.py
@@ -0,0 +1,26 @@
+HOST="localhost"
+PORT=9000
+HEADER_SIZE=10
+MAX_QUEUE=0
+MAX_CONNECTIONS=1
+BUFFER_SIZE=1024
+
+def send_message(receiver, message, encode=True):
+    message = str(message)
+    packet = f"{len(message):<{HEADER_SIZE}}" + message
+    if encode:
+        packet = packet.encode("utf-8")
+    receiver.send(packet)
+def receive_message(receiver, encode=True):
+    message = ''
+    new_packet = True
+    while True:
+        packet = receiver.recv(BUFFER_SIZE)
+        if new_packet:
+            packet_length = int(packet[:HEADER_SIZE])
+            new_packet = False
+        if encode:
+            packet = packet.decode("utf-8")
+        message += packet
+        if len(message)-HEADER_SIZE == packet_length:
+            return str(message[HEADER_SIZE:])
--- Pong/main.py
+++ Pong/main.py
@@ -1,16 +1,17 @@
-from asyncio.windows_events import NULL
 from tkinter import *
 from socket import *
 import threading
+import _thread as thread
 import math
-
+import lib.custom_networking as cn
+#import screeninfo
 
 
 class Application(Frame):
     def __init__(self, master, frame_target):
         Frame.__init__(self, master)
         self.grid(sticky=N+W+S+E)
-        self.screen_width, self.screen_height =10,10 #screeninfo.get_monitors()[0].width, screeninfo.get_monitors()[0].height
+        self.screen_width, self.screen_height = 1920, 1080 #screeninfo.get_monitors()[0].width, screeninfo.get_monitors()[0].height
         self.frame_time = math.floor(1000 / frame_target)
     
     def clear_frame(self):
@@ -36,14 +37,16 @@ class Application(Frame):
         self.clear_frame()
         self.main_window.columnconfigure(0, weight=1)
         self.main_window.rowconfigure(0, weight=1)
-        self.draw_space = Canvas(self.main_window, width=self.screen_width/2, height=self.screen_height/2)
+        self.player1 = Player(self.screen_height/2)
+        self.player2 = Player(self.screen_height/2)
+        self.draw_space = Canvas(self.main_window, width=self.screen_width, height=self.screen_height)
         self.draw_space.grid(sticky=N+W+S+E)
-        self.player1 = Player(self.window_height/2)
-        self.player2 = Player(self.window_height/2)
+        
         self.game_loop()
 
     def game_loop(self):
         self.draw_space.delete(ALL)
+        #receive other players y position
         self.draw_players()
         self.master.after(self.frame_time, self.game_loop)
 
@@ -53,6 +56,10 @@ class Application(Frame):
         self.draw_space.create_rectangle(self.player1.x_pos, self.player1.y_pos, self.player1.x_pos + self.player_width, self.player1.y_pos + self.player_height, fill="#0000ff")
         self.draw_space.create_rectangle(self.player2.x_pos - self.player_width, self.player2.y_pos, self.player2.x_pos, self.player2.y_pos + self.player_height, fill="#ff0000")
 
+    # def draw_ball(self,Ball_speed_x, Ball_speed_y):
+    #     Ball_thing = self.create_oval(10,10,50,50,fill = "black")
+        #https://youtu.be/XFU7FC-i-_Y til resten af lortet jeg mangler
+
     def calculate_player_size(self):
         self.player_width = self.window_width * 0.01
         self.player_height = self.window_height * 0.1
@@ -61,8 +68,8 @@ class Application(Frame):
         self.window_width, self.window_height = self.main_window.winfo_width(), self.main_window.winfo_height()
 
     def configure_event(self, event):
-        #self.window_width, self.window_height = event.width, event.height
-        self.window_size()
+        self.window_width, self.window_height = event.width, event.height
+        #self.window_size()
         self.calculate_player_size()
 
     def inputs(self, event):
@@ -77,15 +84,27 @@ class Application(Frame):
                     self.player1.move(1)
 
     def start_server(self):
-        threading.Thread(target=lambda : Server(self)).start()
         self.game_window()
+        thread.start_new_thread(Server, (self))
     
     def start_client(self):
-        threading.Thread(target=Client).start()
         self.game_window()
+        #threading.Thread(target=lambda : Client(self)).start()
+        thread.start_new_thread(Client, (self))
 
 
+# class Ball:
+#     def __init__(self):
+#         Ball_speed_x = 3
+#         Ball_speed_y = 3
 
+#     def moveBall(self,Ball_thing):
+#         self.move(Ball_thing,Ball_speed_x,Ball_speed_y)
+#         (left_pos,top_pos,right_pos,bottom_pos) = self.coords()
+#         if left_pos <= 0 or right_pos >= 100:#fjern 100, det er en placeholder
+#             Ball_speed_x = -Ball_speed_x
+#         if top_pos <= 0 or bottom_pos >= 100: #--||--
+#             Ball_speed_y = -Ball_speed_y
 
 class Player:
     def __init__(self, start_height):
@@ -94,7 +113,7 @@ class Player:
         self.speed = 5
     def move(self, inputs):
         self.y_pos += inputs * self.speed
-        
+
 
 
 class Server:
@@ -102,15 +121,14 @@ class Server:
         # print(self.LANG['startup_message'].format(self.SERVER_CONFIG['port']))
         print("Starting server!")
         self.server_socket = socket(AF_INET, SOCK_STREAM)
-        self.server_socket.bind(("localhost", 9000))
+        self.server_socket.bind((cn.HOST, cn.PORT))
         self.server_socket.listen(0)
         self.current_connections = 0
         print("server started!")
         # print(self.LANG['started_message'].format(self.SERVER_CONFIG['host'], self.SERVER_CONFIG['port']))
         while True:
             if self.current_connections < 1:
-                client, client_address = self.server_socket.accept()
-                print("grim")
+                self.client, client_address = self.server_socket.accept()
                 # print(self.LANG['connected_message'].format(client_address))
 
 
@@ -121,11 +139,12 @@ class Server:
 
 
                 self.current_connections += 1
-                print(self.current_connections)
+                print("Current connections:", self.current_connections)
                 # print(self.LANG['connection_count'].format(current_connections))
             else:
-                self.server_socket.sendall(app.player1.y_pos)
-                app.player2.y_pos = client.recv(28)
+                #self.client.sendall(str(app.player1.y_pos).encode("utf-8"))
+                #app.player2.y_pos = float(self.client.recv(24).decode("utf-8"))
+                cn.send_message(self.client, app.player1.y_pos)
         
 
 
@@ -134,8 +153,12 @@ class Client:
         self.client_socket = socket(AF_INET, SOCK_STREAM)
         self.client_socket.connect(("localhost", 9000))
         while self.client_socket is not None:
-            app.player2.y_pos = self.client_socket.recv(28)
-            self.server_socket.sendall(app.player1.y_pos)
+            package = cn.receive_message(self.client_socket)
+            package=float(package)
+            app.player2.y_pos = package
+            print(package, type(package))
+            #self.client_socket.sendall(str(app.player1.y_pos).encode("utf-8"))
+
 
 
 def main():
